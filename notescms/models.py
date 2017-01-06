ad6cb8e98852d28359d35384426481e832cde58b
Michael Lee <michaelsunglee@gmail.com>
2017-01-06 14:48:58 -0500

    Added note to Note model as textfield. Overwrote str print method in Note class as well

--- notescms/models.py
+++ notescms/models.py
@@ -4,5 +4,9 @@ from django.db import models
 class Note(models.Model):
     source_title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
+    note = models.TextField()
     page_number = models.IntegerField()
     date = models.DateTimeField('date added')
+
+    def __str__(self):
+        return "{} by {} in {}".format(self.note, self.author, self.source_title)
