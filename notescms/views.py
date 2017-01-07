b78d4fefa3e2046732ca8fbb36624021f6574934
Michael Lee <michaelsunglee@gmail.com>
2017-01-07 17:56:59 -0500

    basic data passing to index.html to display notes in table

--- notescms/views.py
+++ notescms/views.py
@@ -1,2 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Note
 
+
+class NotesView(generic.ListView):
+    template_name = 'notescms/index.html'
+    context_object_name = 'notes_list'
+
+    def get_queryset(self):
+        return Note.objects.all()
