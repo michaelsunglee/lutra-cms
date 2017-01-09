80b96552a031474b8c4c8dc4f1e1d9368a8d97b3
Michael Lee <michaelsunglee@gmail.com>
2017-01-08 21:51:15 -0500

    Use bootstrap template for table

--- notescms/tables.py
+++ notescms/tables.py
@@ -5,3 +5,5 @@ from .models import Note
 class NoteTable(tables.Table):
     class Meta:
         model = Note
+        template = 'django_tables2/bootstrap.html'
+        attrs = {'class': 'table table-bordered table-striped table-hover'}
